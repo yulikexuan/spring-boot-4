42e18fe1597e3344af9d9956163771346d2eaac7
yulikexuan <yulikexuan@gmail.com>
2026-01-23 12:25:41 -0500

    Adding new di module

---
+++ di/src/main/java/spring/boot/sfg7/SpringBoot4DiApplication.java
@@ -0,0 +1,15 @@
+package spring.boot.sfg7;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBoot4DiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot4DiApplication.class, args);
+    }
+
+}
---
+++ di/src/test/java/spring/boot/sfg7/SpringBoot4DiApplicationIT.java
@@ -0,0 +1,17 @@
+package spring.boot.sfg7;
+
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@Disabled
+@SpringBootTest
+class SpringBoot4DiApplicationIT {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
